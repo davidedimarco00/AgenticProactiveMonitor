eb868e9edd8adde533d56f64db967c1d7e58e2ec
Davide Di Marco <74374315+davidedimarco00@users.noreply.github.com>
2026-08-12 15:00:49 +0200

    Add agent communication package

---
+++ src/agentic_system/agentic_system/communication/__init__.py
@@ -0,0 +1,15 @@
+from .messages import (
+    AGENTIC_PROTOCOL,
+    AgentMessage,
+    Performative,
+    build_spade_message,
+    parse_spade_message,
+)
+
+__all__ = [
+    "AGENTIC_PROTOCOL",
+    "AgentMessage",
+    "Performative",
+    "build_spade_message",
+    "parse_spade_message",
+]
