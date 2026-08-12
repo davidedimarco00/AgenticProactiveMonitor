8b44b160c1e90fe60784ce92a86638b3b2b50ebb
Davide Di Marco <74374315+davidedimarco00@users.noreply.github.com>
2026-08-12 14:23:57 +0200

    Add SPADE agent package

---
+++ src/agentic_system/agentic_system/agents/__init__.py
@@ -0,0 +1,17 @@
+from .base import BaseRoleAgent
+from .roles import (
+    ApplicationEngineerAgent,
+    NetworkEngineerAgent,
+    SoftwareDeveloperAgent,
+    SystemEngineerAgent,
+    TechnicalLeadAgent,
+)
+
+__all__ = [
+    "BaseRoleAgent",
+    "TechnicalLeadAgent",
+    "SystemEngineerAgent",
+    "NetworkEngineerAgent",
+    "ApplicationEngineerAgent",
+    "SoftwareDeveloperAgent",
+]
